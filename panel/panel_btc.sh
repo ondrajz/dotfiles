f6faf9aa01e1ebd1d2a77f4b120228d17431e04b
TrueFurby <truefurby@gmail.com>
2016-11-25 13:35:01 +0100

    add unconfirmed transactions

--- panel/panel_btc.sh
+++ panel/panel_btc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 BTC=$(curl -s https://blockchain.info/ticker | jq -r '.EUR.last')
-TRANS=$(curl -s https://blockchain.info/q/24hrtransactioncount)
-TRANS=`echo $(($TRANS/1000))`
+TRANS_24=$(curl -s https://blockchain.info/q/24hrtransactioncount)
+TRANS_UNC=$(curl -s https://blockchain.info/q/unconfirmedcount)
+TRANS_24=`echo $(($TRANS_24/1000))`
+TRANS_UNC=`echo $(($TRANS_UNC/1000))`
 
 echo "<txt>btc: <span fgcolor='darkkhaki'>€${BTC}</span>
-trans: <span fgcolor='darkslategray4'>${TRANS}k</span></txt>"
+tran: <span fgcolor='darkslategray3'>${TRANS_24}k</span> <span fgcolor='gray'>(${TRANS_UNC}k)</span></txt>"
 
