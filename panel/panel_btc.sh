7cb1449e9d1a7f4289a474d70682e46ffdc21d0d
TrueFurby <truefurby@gmail.com>
2018-09-10 20:01:14 +0200

    Round numbers for BTC

--- panel/panel_btc.sh
+++ panel/panel_btc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 BTC=$(curl -s https://blockchain.info/ticker | jq -r '.EUR.last')
+BTC_ROUND=$(LC_NUMERIC="en_US.UTF-8" printf '%.1f' "${BTC}")
 TRANS_24=$(curl -s https://blockchain.info/q/24hrtransactioncount)
 TRANS_UNC=$(curl -s https://blockchain.info/q/unconfirmedcount)
 TRANS_24=`echo $(($TRANS_24/1000))`
-TRANS_UNC=`echo $(($TRANS_UNC/1000))`
+if [ ${TRANS_UNC} -gt 9999 ]; then
+    TRANS_UNC=`echo $(($TRANS_UNC/1000))k`
+fi
 
-echo "<txt>btc: <span fgcolor='darkkhaki'>€${BTC}</span>
-tran: <span fgcolor='darkslategray3'>${TRANS_24}k</span> <span fgcolor='lightgray'>(${TRANS_UNC}k)</span></txt>"
+echo "<txt>btc: <span fgcolor='darkkhaki'>${BTC_ROUND}€</span>
+tran: <span fgcolor='darkslategray3'>${TRANS_24}k</span> <span fgcolor='lightgray'>(${TRANS_UNC})</span></txt>"
 
